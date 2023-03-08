5d26b25890201ade3b8cbef65119cf7a51ab58d3
root <mngenge@gmail.com>
2023-03-08 03:50:40 -0800

    third commit

---
+++ models/amenity.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""crating a Module for Amenity class."""
+
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """Class representing a Amenity."""
+    name = ""
